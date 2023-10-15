49d55eb98159250160509bb6e05abe19e50d3d77
joshuaenoch <joshuaechow@gmail.com>
2023-10-15 00:58:20 -0700

    working home page

--- CareConnect/src/pages/HomePage.jsx
+++ CareConnect/src/pages/HomePage.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+
+    const navigate = useNavigate();
+    const handleClick = (path) => {
+        navigate(path);
+    };
+
     return (
         <div className="container">
-            <h1>Welcome _user name_!</h1>
-
+            <h2 className = "title">Welcome _user name_!</h2>
+            <div className = "ofthedays">
+                <div>Memory of the day:</div>
+                <div style={{marginBottom:"20px", textAlign:"center"}}>"Today I played bingo at the mall with my granddaughter" - 9/4/2023</div>
+            </div>
+            <div className = "ofthedays">
+                <div>Activity of the day:</div>
+                <button
+                    style={{ backgroundColor: "darkblue", color: "white", marginTop: "10px", marginBottom: "20px" }}
+                    onClick={() => handleClick("/sudoku")}
+                >
+                    Play sudoku
+                </button>
+            </div>
+            <div>__caregiver__ would love to talk!</div>
+            <button
+                style={{ backgroundColor: "darkblue", color: "white", marginTop: "10px", marginBottom: "10px" }}
+                onClick={() => handleClick("/CaregiverPage")}
+            >
+                Message __caregiver__
+            </button>
         </div>
     );
 }
