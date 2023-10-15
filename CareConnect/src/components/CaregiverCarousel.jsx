ded41629b843edbe55825eaeb68e8ade12bdc669
Y-Diallo <66224887+Y-Diallo@users.noreply.github.com>
2023-10-14 18:26:03 -0700

    removed slick carosel library
    Co-authored-by: veraguber <veraguber@users.noreply.github.com>
    Co-authored-by: Joshua <joshuaenoch@users.noreply.github.com>
    Co-authored-by: jrahman03 <jrahman03@users.noreply.github.com>

--- CareConnect/src/components/CaregiverCarousel.jsx
+++ CareConnect/src/components/CaregiverCarousel.jsx
@@ -1,13 +1,9 @@
 import { Card } from "@mui/material";
 import Image from "mui-image";
 import React from "react";
-import Slider from "react-slick";
+import Carousel from "react-material-ui-carousel";
 
 function CaregiverCarousel(props) {
-  let settings = {
-
-  };
-
   const getStars = (rating) => {
     if (rating >= 5) {
       return "⭐⭐⭐⭐⭐";
@@ -22,25 +18,22 @@ function CaregiverCarousel(props) {
     return stars;
   };
   return (
-    <>
-      <Slider
-      dots= {true}
-      infinite= {true}
-      speed= {500}
-      slidesToShow= {1}
-      slidesToScroll= {1}
-      >
-        {props.caregivers.map((item, i) => (
-          <div>
-            <Card key={i}>
-              <Image src={item.image} />
-              <h3>{item.name}</h3>
-              {getStars(item.rating)}
-            </Card>
-          </div>
-        ))}
-      </Slider>
-    </>
+    <Carousel
+      animation="slide"
+      cycleNavigation={true}
+      navButtonsAlwaysVisible={true}
+      fullHeightHover={true}
+      autoPlay={false}
+
+    >
+      {props.caregivers.map((item, i) => (
+        <Card key={i}>
+          <Image src={item.image} />
+          <h3>{item.name}</h3>
+          {getStars(item.rating)}
+        </Card>
+      ))}
+    </Carousel>
   );
 }
 
